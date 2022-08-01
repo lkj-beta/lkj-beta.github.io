8ab9c81d04802f48ab9b43879fba8b25a298de0e
Will Hack <will.j.hack@gmail.com>
2022-08-01 09:59:18 -0400

    Update title, boilerplate videos

--- src/Videos.jsx
+++ src/Videos.jsx
@@ -1,7 +1,25 @@
-export default function Videos() {
+import { useContent } from './content';
+
+export default function Contact() {
+  const content = useContent('videos');
   return (
-    <main id="main-cell-home">
-      <p className="index-txt">Watch ME!</p>
+    <main
+      style={{
+        backgroundImage: 'URL(images/interior-bgd.jpg)',
+        minHeight: '300px',
+      }}
+    >
+      <p className="index-txt">Watch Me Now</p>
+      <h1 style={{ color: 'white' }}>{content.hi}</h1>
+      <iframe
+        title="LJK video player"
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/Aai-U8npvdM"
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
     </main>
   );
 }

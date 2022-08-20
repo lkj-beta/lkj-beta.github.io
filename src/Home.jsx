8bfef60ecdbb56bbc33085d7e3b9370ad6a8d919
Will Hack <will.j.hack@gmail.com>
2022-08-19 23:16:42 -0400

    Add pictures. Round out `Home` page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,23 @@
 import { useContent } from './content';
+import headshot from '../public/images/home-headshot.jpeg'; // 512 x 640
+import atPiano from '../public/images/larry-king-2.jpg';
 
 export default function Home() {
   const content = useContent('home');
 
-  return <main id="main-cell-home">{content}</main>;
+  return (
+    <main style={{ display: 'flex', gap: '8vw' }}>
+      <section style={{ width: '50%', padding: '12px' }}>
+        {content}
+        <img style={{ marginTop: '24px' }} src={atPiano} alt="Larry King playing piano" />
+      </section>
+      <section style={{ width: '50%', zIndex: '-1', marginRight: '12px' }}>
+        <img
+          style={{ opacity: '0.8', marginTop: '12px' }}
+          src={headshot}
+          alt="Larry King smiling"
+        />
+      </section>
+    </main>
+  );
 }
--- src/content.jsx
+++ src/content.jsx
@@ -15,10 +15,10 @@ const copy = {
     home: (
       <p>
         <mark>LARRY KING</mark> is a highly regarded American jazz PIANIST-SINGER who began
-        appearing in leading Manhattan night spots and cabarets as a teenager in the 1970s and has
-        performed continuously over four decades at an amazing variety of top New York venues with{' '}
-        <em>The Larry King Jazz Trio</em> and his big dance band
-        <em>The Larry King Orchestra</em>. <a href="/about">READ MORE</a>
+        appearing at leading Manhattan night spots and cabarets as a teenager and has performed
+        continuously over five decades at an amazing variety of top New York venues with{' '}
+        <em>The Larry King Jazz Trio</em> and his big dance band <em>The Larry King Orchestra</em>.{' '}
+        <a href="/about">READ MORE</a>
       </p>
     ),
     videos: {
@@ -38,12 +38,11 @@ const copy = {
     audio: {},
     home: (
       <p lang="fr">
-        <mark>LARRY KING</mark> est un pianiste et chanteur de jazz tr&egrave;s
-        appr&eacute;ci&eacute; qui a commenc&eacute; sa carri&egrave;re dans les bo&icirc;tes de
-        nuit et les cabarets cot&eacute;s de Manhattan d&egrave;s son adolescence dans les
-        ann&eacute;es 70. Il se produit r&eacute;guli&egrave;rement depuis 40 ans sur les
-        sc&egrave;nes les plus fameuses de New York avec <em>Le Larry King Jazz Trio</em> et son
-        grand orchestre de danse, <em>Le Larry King Orchestra</em>.
+        <mark>LARRY KING</mark> est un pianiste et chanteur de jazz Américain très apprécié qui a
+        commencé sa carrière dans les boîtes de nuit et les cabarets cotés de Manhattan dès son
+        adolescence. Il se produit régulièrement pendant de nombeuses années sur les scènes les plus
+        célèbres et variées de New York avec <em>The Larry King Jazz Trio</em> et son grand
+        orchestre de danse, <em>The Larry King Orchestra</em>.
         <a href="/about">&nbsp;LIRE LA SUITE</a>
       </p>
     ),

f8a5a3cc7c055faa081bdd69895e4d7322bcd204
Will Hack <will.j.hack@gmail.com>
2022-08-22 23:36:25 -0400

    Finish `Contact` and `Videos` pages

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,7 +1,32 @@
+import blueHeadshot from '/images/blue.jpg';
+
 export default function Contact() {
   return (
-    <main id="main-cell-home">
-      <p className="index-txt">Call ME!</p>
+    <main
+      style={{
+        display: 'flex',
+        gap: '10vw',
+        padding: '0 5vw 0 4vw',
+      }}
+    >
+      <article className="contactPage" style={{ width: '50%' }}>
+        <h2 style={{ marginBottom: 'min(3vw, 25px)' }}>CONTACT</h2>
+        <p>
+          <span>Larry King Productions, Inc.</span>
+        </p>
+        <p>255 West 23rd Street</p>
+        <p>Suite #2C-W</p>
+        <p>New York, NY 10011 USA</p>
+        <p>
+          TEL <a href="tel:+12126273282">+1 212 627 3282</a>
+        </p>
+        <p>
+          EMAIL <a href="mailto:LKingMusic@gmail.com">LKingMusic@gmail.com</a>
+        </p>
+      </article>
+      <article style={{ width: '50%' }}>
+        <img style={{ opacity: '0.9' }} src={blueHeadshot} alt="Larry in a blue light" />
+      </article>
     </main>
   );
 }
--- src/Videos.jsx
+++ src/Videos.jsx
@@ -1,25 +1,31 @@
 import { useContent } from './content';
 
+const VideoWrapper = props => (
+  <div className="videoWrapper">
+    <iframe
+      title="LJK video player"
+      width="560"
+      height="315"
+      frameBorder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      {...props}
+    />
+  </div>
+);
+
 export default function Contact() {
   const content = useContent('videos');
   return (
-    <main
-      style={{
-        backgroundImage: 'URL(images/interior-bgd.jpg)',
-        minHeight: '300px',
-      }}
-    >
-      <p className="index-txt">Watch Me Now</p>
-      <h1 style={{ color: 'white' }}>{content.hi}</h1>
-      <iframe
-        title="LJK video player"
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/Aai-U8npvdM"
-        frameBorder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+    <main>
+      <h2>{content.title}</h2>
+      <div style={{ padding: '0 min(42px, 5vw)' }}>
+        <VideoWrapper src="https://www.youtube.com/embed/Gyis1-ghv2E" />
+        <VideoWrapper src="https://www.youtube.com/embed/PDLUxvvlFQc" />
+        <VideoWrapper src="https://www.youtube.com/embed/9FYeqjCA_wU" />
+        <VideoWrapper src="https://www.youtube.com/embed/Aai-U8npvdM" />
+        <VideoWrapper src="https://www.youtube.com/embed/lerdG0iCzt0" />
+      </div>
     </main>
   );
 }

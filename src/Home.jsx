6c4698ee3df81b8d43620e5f6927a2e9cc910252
Will Hack <will.j.hack@gmail.com>
2022-08-22 23:36:38 -0400

    Fix `Home` page imports

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,12 @@
 import { useContent } from './content';
-import headshot from '../public/images/home-headshot.jpeg'; // 512 x 640
-import atPiano from '../public/images/larry-king-2.jpg';
+import headshot from '/images/home-headshot.jpeg';
+import atPiano from '/images/larry-king-2.jpg';
 
 export default function Home() {
   const content = useContent('home');
 
   return (
-    <main style={{ display: 'flex', gap: '8vw' }}>
+    <main style={{ display: 'flex', gap: '8vw', marginTop: '-60px' }}>
       <section style={{ width: '50%', padding: '12px' }}>
         {content}
         <img style={{ marginTop: '24px' }} src={atPiano} alt="Larry King playing piano" />

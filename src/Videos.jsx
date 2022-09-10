56cc66e492586987cf130190305d64e9f1f74140
Will Hack <will.j.hack@gmail.com>
2022-09-10 16:00:49 -0400

    Update video order, allow french player descriptions

--- src/Videos.jsx
+++ src/Videos.jsx
@@ -1,4 +1,6 @@
+import { useContext } from 'react';
 import { useContent } from './content';
+import { LanguageContext } from './LanguageContext';
 
 const VideoWrapper = props => (
   <div className="videoWrapper">
@@ -16,15 +18,17 @@ const VideoWrapper = props => (
 
 export default function Contact() {
   const content = useContent('videos');
+  const { language } = useContext(LanguageContext);
+  const frPlayer = language === 'fr' ? '&hl=fr' : '';
   return (
     <main>
       <h2>{content.title}</h2>
       <div style={{ padding: '0 min(42px, 5vw)' }}>
-        <VideoWrapper src="https://www.youtube.com/embed/Gyis1-ghv2E" />
-        <VideoWrapper src="https://www.youtube.com/embed/PDLUxvvlFQc" />
-        <VideoWrapper src="https://www.youtube.com/embed/9FYeqjCA_wU" />
-        <VideoWrapper src="https://www.youtube.com/embed/Aai-U8npvdM" />
-        <VideoWrapper src="https://www.youtube.com/embed/lerdG0iCzt0" />
+        <VideoWrapper src={`https://www.youtube.com/embed/PDLUxvvlFQc?rel=0${frPlayer}`} />
+        <VideoWrapper src={`https://www.youtube.com/embed/9FYeqjCA_wU?rel=0${frPlayer}`} />
+        <VideoWrapper src={`https://www.youtube.com/embed/Aai-U8npvdM?rel=0${frPlayer}`} />
+        <VideoWrapper src={`https://www.youtube.com/embed/lerdG0iCzt0?rel=0${frPlayer}`} />
+        <VideoWrapper src={`https://www.youtube.com/embed/Gyis1-ghv2E?rel=0${frPlayer}`} />
       </div>
     </main>
   );
